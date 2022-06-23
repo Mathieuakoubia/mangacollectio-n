72089e6d923d35482658b5d5dbf49cdcb2184459
mathieu <mathieu.akoubia@epsi.fr>
2022-06-23 12:50:26 +0200

    CHANGEMENT

---
+++ src/collection.js
@@ -0,0 +1,19 @@
+import React, {useState,useEffect} from "react";
+import "./collection.css"
+
+export default function collection_item(){
+    return(
+        <div class="collection-container">
+            <img src="img.jpg" alt="couverture manga">
+                <div class="description">
+                    <h3>Title</h3>
+                    <ul>
+                        <li>nb tome</li>
+                        <li>auteur</li>
+                        <li>date</li>
+                    </ul>
+                    <a href="/">voir plus</a>
+                </div>
+        </div>
+)
+}

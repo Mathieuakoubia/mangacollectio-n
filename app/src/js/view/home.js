71e395254df92786b3c78d3828d864ac5391021e
julien belinga <julienbelinga29@gmail.com>
2022-07-05 10:12:15 +0200

    code cleaned

---
+++ app/src/js/view/home.js
@@ -0,0 +1,12 @@
+import React, {useState,useEffect} from "react";
+import Collection from '../component/collection_item';
+
+// const test = [{title : 1}, {title : 2}, {title : 3}];s
+
+export default function Home(){
+    return(
+        <div>
+            <Collection />
+        </div>
+    )
+}
